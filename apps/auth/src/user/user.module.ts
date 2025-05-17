4937d27e586e9f06bf13cd4a79d893c28473fd3e
Yangsubo <sheeep555@gmail.com>
2025-05-17 17:25:11 +0900

    Feat: Configure UserModule with JWT strategy and multiple controllers

--- apps/auth/src/user/user.module.ts
+++ apps/auth/src/user/user.module.ts
@@ -6,17 +6,22 @@ import { UserService } from "./user.service";
 import { JwtModule } from "@nestjs/jwt";
 import { JwtStrategy } from "../auth/jwt.strategy";
 import { UserRoleController } from "./user-role.controller";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: "1h" },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>("JWT_SECRET"),
+        signOptions: { expiresIn: "1h" },
+      }),
     }),
   ],
   controllers: [UserController, UserRoleController],
   providers: [UserService, JwtStrategy],
-  exports: [UserService],
+  exports: [UserService, JwtModule],
 })
 export class UserModule {}
